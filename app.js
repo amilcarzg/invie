b2fbc21528dbaf8f708e61b97a4ad781c4ad93ae
Amilcar Zambrano <amilcarzg@gmail.com>
2017-11-16 20:32:03 -0500

    Se hagrega menu hamburguesa

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-webpackJsonp([0],[
-/* 0 */
-/***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
+var consulta = window.matchMedia('(max-width:800px)');
+var $burguerButton = document.getElementById("burger-button");
+var $menu = document.getElementById("menu");
 
+consulta.addListener(mediaQuery);
 
-var css = __webpack_require__(1);
+function mediaQuery(){
+    if (consulta.matches) {
+        console.log('media cumple')
+        $burguerButton.addEventListener('touchstart', toogleMenu);
+    }
+    else {
+        $burguerButton.removeEventListener('touchstart', toogleMenu);
+        console.log('00');
+    }
+}
 
-console.log(css);
+mediaQuery();
 
-/***/ }),
-/* 1 */
-/***/ (function(module, exports) {
+function toogleMenu(e) {
+    $menu.classList.toggle('active');
+    $burguerButton.classList.toggle('icon-close');
 
-// removed by extract-text-webpack-plugin
+}
 
-/***/ })
-],[0]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvanMvYXBwLmpzIiwid2VicGFjazovLy8uL3NyYy9jc3MvYXBwLmNzcyJdLCJuYW1lcyI6WyJjc3MiLCJyZXF1aXJlIiwiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUFBLElBQU1BLE1BQU0sbUJBQUFDLENBQVEsQ0FBUixDQUFaOztBQUVBQyxRQUFRQyxHQUFSLENBQVlILEdBQVosRTs7Ozs7O0FDRkEseUMiLCJmaWxlIjoiYXBwLmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgY3NzID0gcmVxdWlyZSgnLi4vY3NzL2FwcC5jc3MnKVxuXG5jb25zb2xlLmxvZyhjc3MpXG5cblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gLi9zcmMvanMvYXBwLmpzIiwiLy8gcmVtb3ZlZCBieSBleHRyYWN0LXRleHQtd2VicGFjay1wbHVnaW5cblxuXG4vLy8vLy8vLy8vLy8vLy8vLy9cbi8vIFdFQlBBQ0sgRk9PVEVSXG4vLyAuL3NyYy9jc3MvYXBwLmNzc1xuLy8gbW9kdWxlIGlkID0gMVxuLy8gbW9kdWxlIGNodW5rcyA9IDAiXSwic291cmNlUm9vdCI6IiJ9
+// $menu.classList.add('active')
+// $menu.classList.remove('active')
